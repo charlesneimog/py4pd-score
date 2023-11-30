8cc4ee3ca2491f3187f5fb49ac9fb77ff8d6d2a1
Charles K. Neimog <charlesneimog@outlook.com>
2023-11-30 15:21:46 -0800

    fixes for mac

--- pd-score.py
+++ pd-score.py
@@ -4,7 +4,6 @@ from src.chord import chord
 
 def py4pdLoadObjects():
     patchZoom = pd.get_patch_zoom()
-    pd.print(patchZoom)
     if (patchZoom == 1):
         scoreImage = './resources/score.gif'
     elif (patchZoom == 2):
@@ -12,6 +11,4 @@ def py4pdLoadObjects():
     else:
         pd.error("The zoom of the patch is other than 1 or 2, using the default image")
         scoreImage = './resources/score.png'
-    pd.add_object(chord, 'py.chord', objtype=pd.VIS), objimage=scoreImage)
-        
-    pd.add_object(chord, 'py.chord', objtype=pd.VIS, objimage=scoreImage)
+    pd.add_object(chord, 'py.chord', objtype=pd.VIS objimage=scoreImage)
